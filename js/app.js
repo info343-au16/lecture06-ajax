4e6355b067349316f5c489c101ac71c7ca1a83b4
Joel Ross <joelross@uw.edu>
2016-10-24 10:23:22 -0700

    End of lecture code

--- js/app.js
+++ js/app.js
@@ -15,6 +15,11 @@ var songs = [{
     preview_url: "https://p.scdn.co/mp3-preview/8f1e48f29b418c274ce96fd6ae0f6b5002aefe02",
   }];
 
+
+var records = document.querySelector('.records');
+
+var nowPlaying = new Audio();
+
 /**
  * This function takes in an object representing a SINGLE music track
  * and displays it on the page.
@@ -24,18 +29,17 @@ function renderTrack(track){
   //select the `.records` section that will contain the records
   //you could also do this before the function declaration
 
-
   //create a new `img` element to visually represent the track
-
+  var record = document.createElement('img');
 
   //set the image's `src` to be the track's `album_image`
-
+  record.setAttribute('src', track.album.images[0].url);
 
   //always give images `alt` attributes! The `name` of the track will work fine
-
+  record.setAttribute('alt', track.name);
 
   //attach the image element to the `.records` section
-
+  records.appendChild(record);
 
   //add an event listener so that when the track is clicked on,
   //it starts to spin! (add the Font-Awesome utility class `fa-spin`)
@@ -44,10 +48,57 @@ function renderTrack(track){
   //to change the element that was clicked on!
   //
   //Bonus: can you make the track stop spinning when clicked again?
+  record.addEventListener('click', function(event){
+    var source = event.target;
+
+    //same song
+    if(nowPlaying.src === track.preview_url){
+      if(nowPlaying.paused){
+        nowPlaying.play();
+      }
+      else {
+        nowPlaying.pause();
+      }
+    } 
+    else { //different song
+      nowPlaying.pause();
+      nowPlaying = new Audio(track.preview_url);
+      nowPlaying.play();
+    }
+
+
+    source.classList.toggle('fa-spin');
+  });
 
 
 } //end method
 
 //call this function `forEach` element in the `songs` array!
 
+//songs.forEach(renderTrack);
+
+var form = document.querySelector('form');
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  console.log('submitted!');
+
+  var input = document.querySelector('#searchQuery');
+  var userEntry = input.value;
+
+  var url = 'https://api.spotify.com/v1/search?type=track&q='+userEntry;
+
+  fetch(url)
+    .then(function(response){
+      var newPromise = response.json();
+      return newPromise;
+    })
+    .then(function(data){
+      console.log(data);
+      data.tracks.items.forEach(renderTrack);
+    })
+
+
+
+  return false;
+});
 
